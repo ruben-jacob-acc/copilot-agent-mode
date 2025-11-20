13eb11763562cadf259b7843a5b45fa05ca9e6de
ruben-jacob-acc <ruben.jacob@accenture.com>
2025-11-20 08:41:52 +0000

    Fix management command: handle deletion errors, ensure test data population works

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,48 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import Team, User, Activity, Workout, Leaderboard
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        # Clear existing data
+        Activity.objects.all().delete()
+        Workout.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        # Delete User and Team objects individually to avoid unhashable error
+        for user in User.objects.all():
+            if user.id:
+                user.delete()
+        for team in Team.objects.all():
+            if team.id:
+                team.delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users
+        users = [
+            User.objects.create(name='Spider-Man', email='spiderman@marvel.com', team=marvel),
+            User.objects.create(name='Iron Man', email='ironman@marvel.com', team=marvel),
+            User.objects.create(name='Wonder Woman', email='wonderwoman@dc.com', team=dc),
+            User.objects.create(name='Batman', email='batman@dc.com', team=dc),
+        ]
+
+        # Create activities
+        Activity.objects.create(user=users[0], type='Running', duration=30)
+        Activity.objects.create(user=users[1], type='Cycling', duration=45)
+        Activity.objects.create(user=users[2], type='Swimming', duration=60)
+        Activity.objects.create(user=users[3], type='Yoga', duration=50)
+
+        # Create workouts
+        workout1 = Workout.objects.create(name='Hero HIIT', description='High intensity interval training for heroes.')
+        workout2 = Workout.objects.create(name='Power Yoga', description='Yoga for strength and flexibility.')
+        workout1.suggested_for.set([users[0], users[1]])
+        workout2.suggested_for.set([users[2], users[3]])
+
+        # Create leaderboard
+        Leaderboard.objects.create(team=marvel, points=100)
+        Leaderboard.objects.create(team=dc, points=120)
+
+        self.stdout.write(self.style.SUCCESS('octofit_db populated with test data.'))
